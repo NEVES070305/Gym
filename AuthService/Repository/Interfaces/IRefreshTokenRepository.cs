d7b48de8e6bcd0edb8b2885b60b00654ba171952
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-11-24 20:15:27 -0300

    feat(RefreshTokenRepository): added refreshTokenRepository

---
+++ AuthService/Repository/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using AuthService.Models.Refresh;
+
+namespace AuthService.Repository.Interfaces
+{
+    public interface IRefreshTokenRepository
+    {
+       Task SaveRefreshTokenAsync(RefreshTokenModel refreshToken);
+       Task RevokeRefreshTokenAsync(string userId, string refreshToken);
+       Task<RefreshTokenModel> GetRefreshTokenAsync(string userId, string refreshToken);
+    }
+}
---
+++ AuthService/Repository/RefreshTokenRepository.cs
@@ -0,0 +1,49 @@
+﻿using AuthService.Models.Refresh;
+using MongoDB.Driver;
+using AuthService.Repository.Interfaces;
+
+namespace AuthService.Repository
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+        private readonly IMongoCollection<RefreshTokenModel> _refreshTokens;
+
+        public RefreshTokenRepository(IMongoDatabase database)
+        {
+            _refreshTokens = database.GetCollection<RefreshTokenModel>("refresh_tokens");
+
+            // Configurar índice TTL para tokens expirados
+            var indexOptions = new CreateIndexOptions { ExpireAfter = TimeSpan.FromSeconds(0) };
+            var indexKeys = Builders<RefreshTokenModel>.IndexKeys.Ascending(rt => rt.Expiration);
+            _refreshTokens.Indexes.CreateOne(new CreateIndexModel<RefreshTokenModel>(indexKeys, indexOptions));
+        }
+
+        public async Task SaveRefreshTokenAsync(RefreshTokenModel refreshTokenModel)
+        {
+            await _refreshTokens.InsertOneAsync(refreshTokenModel);
+        }
+
+        public async Task<RefreshTokenModel> GetRefreshTokenAsync(string userId, string refreshToken)
+        {
+            return await _refreshTokens
+                .Find(rt => rt.UserId == userId && rt.Token == refreshToken)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task RevokeRefreshTokenAsync(string userId, string refreshToken)
+        {
+            var filter = Builders<RefreshTokenModel>.Filter.Where(rt => rt.UserId == userId && rt.Token == refreshToken);
+            var update = Builders<RefreshTokenModel>.Update.Set(rt => rt.Revoked, true);
+
+            await _refreshTokens.UpdateOneAsync(filter, update);
+        }
+
+        public async Task RevokeAllTokensForUserAsync(string userId)
+        {
+            var filter = Builders<RefreshTokenModel>.Filter.Eq(rt => rt.UserId, userId);
+            var update = Builders<RefreshTokenModel>.Update.Set(rt => rt.Revoked, true);
+
+            await _refreshTokens.UpdateManyAsync(filter, update);
+        }
+    }
+}
