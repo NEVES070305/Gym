1d08535cdc1b0ebafcc1aeb7decd802b6036d3a5
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-11-24 20:10:39 -0300

    feat(IAuthBusiness): added interface of business layer

---
+++ AuthService/Business/Interfaces/IAuthBusiness.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Business.Interfaces
+{
+    public interface IAuthBusiness
+    {
+        Task<(string AccessToken, string RefreshToken)> LoginAsync(string username, string password);
+        Task<(string AccessToken, string RefreshToken)> RefreshAsync(string userId, string refreshToken);
+    }
+}
