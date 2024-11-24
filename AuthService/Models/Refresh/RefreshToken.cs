ddfd064ca5afed60f63513fd5ae9e687fb4f9d49
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-11-24 20:13:31 -0300

    feat(RefreshToken): added a model to be persisted in the bd

---
+++ AuthService/Models/Refresh/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace AuthService.Models.Refresh
+{
+    public class RefreshTokenModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserId { get; set; } 
+        public string Token { get; set; } 
+        public DateTime Expiration { get; set; } 
+        public bool Revoked { get; set; } = false; 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+    }
+}
